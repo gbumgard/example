c31367e1f58b3c989f6807ad907dc6c74b9bddf5
Greg Bumgardner <gbumgard@gmail.com>
2021-03-06 23:57:18 -0800

    Snapshot of SQL resource support.

--- amplify/backend/function/rdsSqlDocumentLoader/src/index.js
+++ amplify/backend/function/rdsSqlDocumentLoader/src/index.js
@@ -5,7 +5,7 @@ Amplify Params - DO NOT EDIT */
 
 process.env.AWS_SDK_LOAD_CONFIG = 1;
 const AWS = require('aws-sdk');
-const CRYPTO = require('crypto');
+const crypto = require('crypto');
 
 const RDS = new AWS.RDSDataService();
 const secretsmanager = new AWS.SecretsManager();
@@ -21,6 +21,7 @@ exports.handler = async (event,context) => {
   try {
 
     if (event.RequestType === "Delete") {
+      // Delete request should always report succes to avoid hanging the stack in a delete or rollback
       await sendCFResponse(event, context, "SUCCESS", {}).catch(e => {});
       return event;
     }
@@ -49,6 +50,7 @@ exports.handler = async (event,context) => {
     var initDb = "DELIMITER " + delimiter + "\n";
 
     if (event.ResourceProperties.Database) {
+      // Create the database if it does not yet exist
       initDb += "CREATE DATABASE IF NOT EXISTS " + event.ResourceProperties.Database + delimiter;
       initDb += "USE " + event.ResourceProperties.Database + delimiter;
     }
@@ -62,28 +64,44 @@ exports.handler = async (event,context) => {
       event.ResourceProperties.CreateSqlDocument = initDb + event.ResourceProperties.CreateSqlDocument;
     }
 
-    physicalResourceId = CRYPTO.createHash('md5').update(event.ResourceProperties.CreateSqlDocument).digest('hex');
+    // Use hash of the Create document as the physical resource ID.
+    // The hash is used to detect changes in the Create document.
+    physicalResourceId = crypto.createHash('md5').update(event.ResourceProperties.CreateSqlDocument).digest('hex');
 
     var sql = "";
 
     if (event.RequestType == "Create") {
+    
       sql = event.ResourceProperties.CreateSqlDocument;
+
+      // The function must return a physical resource ID for the resource.
+      // This value will be sent in the next event and used to check for SQL property changes
       event.PhysicalResourceId = physicalResourceId;
+    
     }
     else if (event.RequestType == "Update") {
+
+      // Use the UpdateSqlDocument property if it exists otherwise use the CreateSqlDocument property.
       if (event.ResourceProperties.UpdateSqlDocument) {
+
         if (event.ResourceProperties.Database) {
           rdsArgs.database = event.ResourceProperties.Database;
         }
+
         sql = event.ResourceProperties.UpdateSqlDocument;
       }
       else if (physicalResourceId != event.PhysicalResourceId) {
-        sql = event.ResourceProperties.CreateSqlDocument;
+
+        // The physical resource ID must be updated when the resource is changed.
+        // This value will be sent in the next event.
         event.PhysicalResourceId = physicalResourceId;
+
+        sql = event.ResourceProperties.CreateSqlDocument;
       }
     }
     else {
-      console.log("ignoring RequestType " + event.requestType);
+      
+      console.log("unexpected RequestType " + event.requestType);
       await sendCFResponse(event, context, "SUCCESS", {}).catch(e => {});
       return event;
     }
@@ -93,25 +111,35 @@ exports.handler = async (event,context) => {
     console.log(statements);
 
     var results = [];
+
+    // Execute each SQL statement
     for (var statement of statements) {
       if (statement.length > 0) {
         rdsArgs.sql = statement;
+
         var result = await executeSql(rdsArgs);
+
+        // Store the SQL statement and the DB server response.
         results.push([rdsArgs.sql,result]);
       }
     }
 
+    // Report success and results. The results may be used as a CF output.
     await sendCFResponse(event, context, "SUCCESS", {results: JSON.stringify(results)} ).catch(e => {});
   
   }
   catch (err) {
+
+    // Catch everything else - the handler must send a SUCCESS response to avoid hanging stack.
     console.log(err);
     await sendCFResponse(event, context, event.RequestType == "Delete" ? "SUCCESS" : "FAILED",{}).catch(e => {});
+
   }
 
   return event;
 };
 
+let whitespace = /\s+/;
 
 function parseStatements(sql, delimiter) {
 
@@ -120,20 +148,29 @@ function parseStatements(sql, delimiter) {
   var statements = [];
 
   while (sql.length > 0) {
+
+    // Strip leading white space
     var c = sql[0];
     if (c === " " || c === "\n") {
       sql = sql.substring(1);
       continue;
     }
   
-    var isDelimiterProperty = false;
+    // Use DELIMITER "statements" to change the delimiter value but don't execute them.
+
+    var isDelimiterCommand = false;
     if (sql.toUpperCase().startsWith("DELIMITER")) {
-      delimiter = sql.split(/\s+/)[1].split('\n')[0];
-      isDelimiterProperty = true;
+      delimiter = sql.split(whitespace)[1].split('\n')[0];
+      isDelimiterCommand = true;
     }
     
+    // Extract the statement from the document string
     var statement = sql.substring(0,sql.indexOf(delimiter)+delimiter.length).replace('\n',' ');
-    if (!isDelimiterProperty) statements.push(statement);
+
+    // Execute if NOT a DELIMITER command
+    if (!isDelimiterCommand) statements.push(statement);
+
+    // Advance to the next statement
     sql = sql.substring(statement.length);
   }
 
@@ -144,20 +181,13 @@ function parseStatements(sql, delimiter) {
 function executeSql(params) {
   return new Promise((resolve, reject) => {
     console.log(["Executing SQL statement:",params.sql]);
-    /*
-    if (params.sql.toUpperCase().startsWith("USE")) {
-      var db = params.sql.split(/\s+/)[1];
-      params.database = db.substring(0,db.length-1);
-      console.log(["Switching to database: "+params.database]);
-    }
-    */
     RDS.executeStatement(params, (err, result) => {
       if (err) {
-        console.warn(["SQL statement execution failed with error:",err]);
+        console.warn("SQL statement '" + params.sql + "' failed with error " + err);
         reject(err)
       }
       else {
-        console.log(["SQL statement execution succeeded with result:",JSON.stringify(result)]);
+        console.log(["SQL statement '" + params.sql + "' returned result:",JSON.stringify(result)]);
         resolve(result);
       }
     });
@@ -201,7 +231,7 @@ function sendCFResponse(event, context, responseStatus, responseData) {
         "statusCode": response.statusCode,
         "statusMessage": response.statusMessage,
         "headers": response.headers,
-      }
+      };
   
       if (response.statusCode < 200 || response.statusCode >= 300) {
         console.log(result)
